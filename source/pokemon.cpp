9cf461f5fa14d80f0673c3afc929341c215b3ff1
gennaro <gen.iorio92@gmail.com>
2019-01-31 17:49:26 +0100

    implement zekrom and reshiram peculiar abilities

--- source/pokemon.cpp
+++ source/pokemon.cpp
@@ -204,11 +204,11 @@ float Pokemon::calculateOtherModifier(const Pokemon& theAttacker, const Move& th
     if( getAbility() == Ability::Shadow_Shield && getCurrentHPPercentage() == 100 ) modifier = modifier * 0.5;
     else if( getAbility() == Ability::Prism_Armor && calculateTypeModifier(theMove) >= 2 ) modifier = modifier * 0.75;
 
-    //these effects are ignored by solgaleo & lunala peculiar moves
-    if( theMove.getMoveIndex() != Moves::Moongeist_Beam && theMove.getMoveIndex() != Moves::Sunsteel_Strike && theMove.getMoveIndex() != Moves::Menacing_Moonraze_Maelstrom && theMove.getMoveIndex() != Moves::Searing_Sunraze_Smash ) {
+    //these effects are ignored by solgaleo & lunala peculiar moves and zekrom e reshiram abilities
+    if( theMove.getMoveIndex() != Moves::Moongeist_Beam && theMove.getMoveIndex() != Moves::Sunsteel_Strike && theMove.getMoveIndex() != Moves::Menacing_Moonraze_Maelstrom && theMove.getMoveIndex() != Moves::Searing_Sunraze_Smash && theAttacker.getAbility() != Ability::Turboblaze && theAttacker.getAbility() != Ability::Teravolt) {
         if( getAbility() == Ability::Wonder_Guard && calculateTypeModifier(theMove) < 2 ) modifier = modifier * 0;
         else if( getAbility() == Ability::Multiscale ) modifier = modifier * 0.5;
-        else if( getAbility() == Ability::Filter || getAbility() == Ability::Solid_Rock ) modifier = modifier * 0.75;
+        else if( (getAbility() == Ability::Filter || getAbility() == Ability::Solid_Rock) && calculateTypeModifier(theMove) > 2  ) modifier = modifier * 0.75;
     }
 
     if( theAttacker.getItem() == Items::Life_Orb ) modifier = modifier * 1.3;
